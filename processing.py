2d90414c22847e89cdc228e2330b995cce2435f6
picklesueat <jgutstadt48@gmail.com>
2021-03-27 17:32:20 -0500

    added source parameters to tiredness model

--- processing.py
+++ processing.py
@@ -72,39 +72,10 @@ def draw_landmarks(face_part, landmarks,frame):
 
 
 def main(source = None):
-    # DLIB - Face Detector and Predictor
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-
-    #if no source, we use live video capture
-    if not source:
-        # Video Capture
-        cap = cv2.VideoCapture(0)
-
-        # Text settings
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 0.7
-
-    # Initializations
-    frames = 0
-
-    # e.a.r & l.a.r, threshold values
-    ear_thresh = 0.3
-    lar_thresh = 0.5
-
-    # Blink initializations
-    blink_counter, total_blinks = 0, 0
-    # Yawn initializations
-    yawn_counter, total_yawns = 0, 0
-
-
-    while True:
-        _, frame = cap.read()
-        frame = cv2.flip(frame, 1)  # May not be necessary
-        h, w = frame.shape[: 2]  # Height and Width of frame
-
-        frames += 1
-
+    def analyze_frame(frame):
+        print('analyze')
+        blinking = False
+        yawning = False
         # Grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -141,45 +112,79 @@ def main(source = None):
             # Calculate E.A.R. and L.A.R.
             left_ear = eye_aspect_ratio(left_eye_points)  # Left eye aspect ratio
             right_ear = eye_aspect_ratio(right_eye_points)  # Right eye aspect ratio
+
             ear = (left_ear + right_ear) / 2.0  # Average eye aspect ratio
-            cv2.putText(frame, "E.A.R. : {:.2f}".format(ear), (10, 30), font, font_scale, (0, 0, 255), 2)
 
             lar = lips_aspect_ratio(lips_points)  # Lips aspect ratio
-            cv2.putText(frame, "L.A.R. : {:.2f}".format(lar), (10, 90), font, font_scale, (0, 0, 255), 2)
 
-            # BLINK detected
             if ear < ear_thresh:
+                blinking = True
+            if lar > lar_thresh:
+                yawning = True
+        return (blinking, yawning)
+
+    # DLIB - Face Detector and Predictor
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    # Initializations
+    frames = 0
+
+    # e.a.r & l.a.r, threshold values
+    ear_thresh = 0.3
+    lar_thresh = 0.5
+
+    # Blink initializations
+    blink_counter, total_blinks = 0, 0
+    # Yawn initializations
+    yawn_counter, total_yawns = 0, 0
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.7
+
+    #if no source, we use live video capture
+    if not source:
+        # Video Capture
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            _, frame = cap.read()
+            frame = cv2.flip(frame, 1)  # May not be necessary
+            h, w = frame.shape[: 2]  # Height and Width of frame
+
+            frames += 1
+
+            blinking, yawning = analyze_frame(frame)
+
+            #cv2.putText(frame, "L.A.R. : {:.2f}".format(lar), (10, 90), font, font_scale, (0, 0, 255), 2)
+            #cv2.putText(frame, "L.A.R. : {:.2f}".format(ear), (10, 90), font, font_scale, (0, 0, 255), 2)
+
+
+            if blinking:
                 blink_counter += 1
             else:
-                if blink_counter > 3:
+                if blink_counter > 1:
                     total_blinks += 1
                 blink_counter = 0
             cv2.putText(frame, "Blinks: {}".format(total_blinks), (10, 50), font, font_scale, (0, 0, 255), 2)
-
             # YAWN detected
-            if lar > lar_thresh:
+            if yawning:
                 yawn_counter += 1
             else:
                 if yawn_counter > 1:
                     total_yawns += 1
                 yawn_counter = 0
             cv2.putText(frame, "Yawns: {}".format(total_yawns), (10, 110), font, font_scale, (0, 0, 255), 2)
+            cv2.imshow('Frame', frame)
+            key = cv2.waitKey(1)
+            # if the user presses the 'esc' key, quit out
+            if key == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
 
-            # show alert text if person yawns
-            # if total_yawns > 2 or total_blinks > 3:
-            #     # frame[20:120, w - 120:w - 20] = image  # Show coffee break image
-            #     cv2.putText(frame, "ALERT", (w - 120, 160), font, 1.2, (0, 0, 255), 4)
-
-        cv2.imshow('Frame', frame)
-        key = cv2.waitKey(1)
-        # if the user presses the 'esc' key, quit out
-        if key == 27:
-            break
-        # if key == ord('r') or key == ord('R'):
-        #     total_blinks, total_yawns = 0, 0  # Reset calculations by pressing 'r' or 'R'
-
-    cap.release()
-    cv2.destroyAllWindows()
+    if source:
+        pass
 
 
 if __name__ == '__main__':
