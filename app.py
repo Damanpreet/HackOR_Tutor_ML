e20f16b451d908622e1771c26354511b5b6f754f
picklesueat <jgutstadt48@gmail.com>
2021-03-27 18:28:56 -0500

    fixed2X

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 from flask.wrappers import Response
 from camera import VideoRecorder
+from .processing import calculate_scores
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
@@ -16,18 +17,39 @@ def index():
 
 def gen(camera):
     global recorder
+    # Blink initializations
+    blink_counter, total_blinks = 0, 0
+    # Yawn initializations
+    yawn_counter, total_yawns = 0, 0
+
+    frames = 0
+
     while recorder:
-        data = camera.get_frame()
-        frame = data
+        frame = camera.get_frame()
+        frames += 1
         if len(frame)==0:
-            print("why?")
+            print("why??")
             break
+        blinking, yawning, ear, lar = calculate_scores(frame)
+        if blinking:
+            blink_counter += 1
+        else:
+            if blink_counter > 8:
+                total_blinks += 1
+            blink_counter = 0
+
+        if yawning:
+            yawn_counter += 1
+        else:
+            if yawn_counter > 1:
+                total_yawns += 1
+
 
 @app.route('/video_feed')
 def video_feed():
     global recorder
     recorder = VideoRecorder()
-    gen(recorder) 
+    gen(recorder)
     return {}
 
 @app.route('/video_stop')
--- processing.py
+++ processing.py
@@ -21,6 +21,9 @@ Landmarks |   0  1  2  3  4  5
 Right Eye : [42,43,44,45,46,47]
 '''
 
+# e.a.r & l.a.r, threshold values
+ear_thresh = 0.3
+lar_thresh = 0.5
 
 def eye_aspect_ratio(eye):
     # Vertical distances
@@ -71,120 +74,62 @@ def draw_landmarks(face_part, landmarks,frame):
 
 
 
-def main(source = None):
-    def analyze_frame(frame):
-        print('analyze')
-        blinking = False
-        yawning = False
-        # Grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def analyze_frame(frame):
+    blinking = False
+    yawning = False
+    # Grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces in the gray frame
-        faces = detector(gray, 0)
+    # Detect faces in the gray frame
+    faces = detector(gray, 0)
 
-        # Loop through each face
-        for face in faces:
-            # Determine facial landmarks
-            facial_landmarks = predictor(gray, face)
+    # Loop through each face
+    for face in faces:
+        # Determine facial landmarks
+        facial_landmarks = predictor(gray, face)
 
-            # Landmark indexes for eyes and lips
-            left_eye = [36, 37, 38, 39, 40, 41]
-            right_eye = [42, 43, 44, 45, 46, 47]
+        # Landmark indexes for eyes and lips
+        left_eye = [36, 37, 38, 39, 40, 41]
+        right_eye = [42, 43, 44, 45, 46, 47]
 
-            lips = [60, 61, 62, 63, 64, 65, 66, 67]
+        lips = [60, 61, 62, 63, 64, 65, 66, 67]
 
-            # Convert to numpy array the above lists and
-            # draw the corresponding facial landmark points on frame
-            left_eye_points = draw_landmarks(left_eye, facial_landmarks,frame)
-            right_eye_points = draw_landmarks(right_eye, facial_landmarks,frame)
-            lips_points = draw_landmarks(lips, facial_landmarks,frame)
+        # Convert to numpy array the above lists and
+        # draw the corresponding facial landmark points on frame
+        left_eye_points = draw_landmarks(left_eye, facial_landmarks,frame)
+        right_eye_points = draw_landmarks(right_eye, facial_landmarks,frame)
+        lips_points = draw_landmarks(lips, facial_landmarks,frame)
 
-            # Find and draw the convex hulls of left and right eye, and lips
-            left_eye_hull = cv2.convexHull(left_eye_points)
-            cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
+        # Find and draw the convex hulls of left and right eye, and lips
+        left_eye_hull = cv2.convexHull(left_eye_points)
+        cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
 
-            right_eye_hull = cv2.convexHull(right_eye_points)
-            cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
+        right_eye_hull = cv2.convexHull(right_eye_points)
+        cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
 
-            lips_hull = cv2.convexHull(lips_points)
-            cv2.drawContours(frame, [lips_hull], -1, (0, 255, 0), 1)
+        lips_hull = cv2.convexHull(lips_points)
+        cv2.drawContours(frame, [lips_hull], -1, (0, 255, 0), 1)
 
-            # Calculate E.A.R. and L.A.R.
-            left_ear = eye_aspect_ratio(left_eye_points)  # Left eye aspect ratio
-            right_ear = eye_aspect_ratio(right_eye_points)  # Right eye aspect ratio
+        # Calculate E.A.R. and L.A.R.
+        left_ear = eye_aspect_ratio(left_eye_points)  # Left eye aspect ratio
+        right_ear = eye_aspect_ratio(right_eye_points)  # Right eye aspect ratio
 
-            ear = (left_ear + right_ear) / 2.0  # Average eye aspect ratio
+        ear = (left_ear + right_ear) / 2.0  # Average eye aspect ratio
 
-            lar = lips_aspect_ratio(lips_points)  # Lips aspect ratio
+        lar = lips_aspect_ratio(lips_points)  # Lips aspect ratio
 
-            if ear < ear_thresh:
-                blinking = True
-            if lar > lar_thresh:
-                yawning = True
-        return (blinking, yawning)
+        if ear < ear_thresh:
+            blinking = True
+        if lar > lar_thresh:
+            yawning = True
+    return (blinking, yawning, ear, lar)
 
+def calculate_scores(frame):
     # DLIB - Face Detector and Predictor
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-    # Initializations
-    frames = 0
-
-    # e.a.r & l.a.r, threshold values
-    ear_thresh = 0.3
-    lar_thresh = 0.5
-
-    # Blink initializations
-    blink_counter, total_blinks = 0, 0
-    # Yawn initializations
-    yawn_counter, total_yawns = 0, 0
-
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 0.7
-
-    #if no source, we use live video capture
-    if not source:
-        # Video Capture
-        cap = cv2.VideoCapture(0)
-
-        while True:
-            _, frame = cap.read()
-            frame = cv2.flip(frame, 1)  # May not be necessary
-            h, w = frame.shape[: 2]  # Height and Width of frame
-
-            frames += 1
-
-            blinking, yawning = analyze_frame(frame)
-
-            #cv2.putText(frame, "L.A.R. : {:.2f}".format(lar), (10, 90), font, font_scale, (0, 0, 255), 2)
-            #cv2.putText(frame, "L.A.R. : {:.2f}".format(ear), (10, 90), font, font_scale, (0, 0, 255), 2)
-
-
-            if blinking:
-                blink_counter += 1
-            else:
-                if blink_counter > 1:
-                    total_blinks += 1
-                blink_counter = 0
-            cv2.putText(frame, "Blinks: {}".format(total_blinks), (10, 50), font, font_scale, (0, 0, 255), 2)
-            # YAWN detected
-            if yawning:
-                yawn_counter += 1
-            else:
-                if yawn_counter > 1:
-                    total_yawns += 1
-                yawn_counter = 0
-            cv2.putText(frame, "Yawns: {}".format(total_yawns), (10, 110), font, font_scale, (0, 0, 255), 2)
-            cv2.imshow('Frame', frame)
-            key = cv2.waitKey(1)
-            # if the user presses the 'esc' key, quit out
-            if key == 27:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-
-    if source:
-        pass
+    return analyze_frame(frame)
 
 
 if __name__ == '__main__':
