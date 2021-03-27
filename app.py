4673da364381220a9291262dc6da663fcfe3760f
Damanpreet <damanpreett@agot.ai>
2021-03-26 22:11:32 -0700

    Video Streaming functionality

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 import os
 from flask import Flask, render_template
+from flask.wrappers import Response
+from camera import VideoRecorder
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
+
+global recorder
+recorder = None
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/video1')
-def video1():
-	return render_template('video1.html')
+def gen(camera):
+    global recorder
+    while recorder:
+        data = camera.get_frame()
+        frame = data
+        if len(frame)==0:
+            print("why?")
+            break
 
-@app.route('/video2') 
-def video2():
-	return render_template('video2.html')
+@app.route('/video_feed')
+def video_feed():
+    global recorder
+    recorder = VideoRecorder()
+    gen(recorder) 
+    return {}
 
-@app.route('/video3')  
-def video3():  
-	return render_template('video3.html')    
+@app.route('/video_stop')
+def video_stop():
+    global recorder
+    del recorder
+    recorder = None
+    print("resource released!")
+    return {}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
