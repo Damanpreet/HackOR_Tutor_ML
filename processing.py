8ee0dcf8f43d1716a4c91ec9b6f95f2a371098ca
picklesueat <jgutstadt48@gmail.com>
2021-03-27 17:01:46 -0500

    made main function

--- processing.py
+++ processing.py
@@ -15,7 +15,7 @@ import cv2
 '''
 Eye Aspect Ratio (E.A.R.)
 Function to calculate eye aspect ratio as in paper :
-"Real-Time Eye Blink Detection using Facial Landmarks [Soukupova, Cech]"             
+"Real-Time Eye Blink Detection using Facial Landmarks [Soukupova, Cech]"
 Landmarks |   0  1  2  3  4  5
  Left Eye : [36,37,38,39,40,41]
 Right Eye : [42,43,44,45,46,47]
@@ -60,8 +60,9 @@ draw them on frame and return a numpy array with the corresponding points
 '''
 
 
-def draw_landmarks(face_part, landmarks):
+def draw_landmarks(face_part, landmarks,frame):
     landmarks_list = []
+    start_frame = frame
     for point in face_part:
         x, y = landmarks.part(point).x, landmarks.part(point).y
         landmarks_list.append([x, y])
@@ -69,116 +70,117 @@ def draw_landmarks(face_part, landmarks):
     return np.array(landmarks_list)
 
 
-# DLIB - Face Detector and Predictor
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-# Video Capture
-cap = cv2.VideoCapture(0)
+def main(source = None):
+    # DLIB - Face Detector and Predictor
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-'''
-Add code around here for grabbing video file from post request? 
-something like... -> 
-data = request.files['videoFile']
-run the code below!
-probably should make into a function
-'''
+    #if no source, we use live video capture
+    if not source:
+        # Video Capture
+        cap = cv2.VideoCapture(0)
 
-# Text settings
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 0.7
+        # Text settings
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.7
 
-# Initializations
-frames = 0
+    # Initializations
+    frames = 0
 
-# e.a.r & l.a.r, threshold values
-ear_thresh = 0.3
-lar_thresh = 0.5
+    # e.a.r & l.a.r, threshold values
+    ear_thresh = 0.3
+    lar_thresh = 0.5
 
-# Blink initializations
-blink_counter, total_blinks = 0, 0
-# Yawn initializations
-yawn_counter, total_yawns = 0, 0
+    # Blink initializations
+    blink_counter, total_blinks = 0, 0
+    # Yawn initializations
+    yawn_counter, total_yawns = 0, 0
 
-while True:
-    _, frame = cap.read()
-    frame = cv2.flip(frame, 1)  # May not be necessary
-    h, w = frame.shape[: 2]  # Height and Width of frame
 
-    frames += 1
+    while True:
+        _, frame = cap.read()
+        frame = cv2.flip(frame, 1)  # May not be necessary
+        h, w = frame.shape[: 2]  # Height and Width of frame
 
-    # Grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frames += 1
 
-    # Detect faces in the gray frame
-    faces = detector(gray, 0)
+        # Grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Loop through each face
-    for face in faces:
-        # Determine facial landmarks
-        facial_landmarks = predictor(gray, face)
+        # Detect faces in the gray frame
+        faces = detector(gray, 0)
 
-        # Landmark indexes for eyes and lips
-        left_eye = [36, 37, 38, 39, 40, 41]
-        right_eye = [42, 43, 44, 45, 46, 47]
+        # Loop through each face
+        for face in faces:
+            # Determine facial landmarks
+            facial_landmarks = predictor(gray, face)
 
-        lips = [60, 61, 62, 63, 64, 65, 66, 67]
+            # Landmark indexes for eyes and lips
+            left_eye = [36, 37, 38, 39, 40, 41]
+            right_eye = [42, 43, 44, 45, 46, 47]
 
-        # Convert to numpy array the above lists and
-        # draw the corresponding facial landmark points on frame
-        left_eye_points = draw_landmarks(left_eye, facial_landmarks)
-        right_eye_points = draw_landmarks(right_eye, facial_landmarks)
-        lips_points = draw_landmarks(lips, facial_landmarks)
+            lips = [60, 61, 62, 63, 64, 65, 66, 67]
 
-        # Find and draw the convex hulls of left and right eye, and lips
-        left_eye_hull = cv2.convexHull(left_eye_points)
-        cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
+            # Convert to numpy array the above lists and
+            # draw the corresponding facial landmark points on frame
+            left_eye_points = draw_landmarks(left_eye, facial_landmarks,frame)
+            right_eye_points = draw_landmarks(right_eye, facial_landmarks,frame)
+            lips_points = draw_landmarks(lips, facial_landmarks,frame)
 
-        right_eye_hull = cv2.convexHull(right_eye_points)
-        cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
+            # Find and draw the convex hulls of left and right eye, and lips
+            left_eye_hull = cv2.convexHull(left_eye_points)
+            cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
 
-        lips_hull = cv2.convexHull(lips_points)
-        cv2.drawContours(frame, [lips_hull], -1, (0, 255, 0), 1)
+            right_eye_hull = cv2.convexHull(right_eye_points)
+            cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
 
-        # Calculate E.A.R. and L.A.R.
-        left_ear = eye_aspect_ratio(left_eye_points)  # Left eye aspect ratio
-        right_ear = eye_aspect_ratio(right_eye_points)  # Right eye aspect ratio
-        ear = (left_ear + right_ear) / 2.0  # Average eye aspect ratio
-        cv2.putText(frame, "E.A.R. : {:.2f}".format(ear), (10, 30), font, font_scale, (0, 0, 255), 2)
+            lips_hull = cv2.convexHull(lips_points)
+            cv2.drawContours(frame, [lips_hull], -1, (0, 255, 0), 1)
 
-        lar = lips_aspect_ratio(lips_points)  # Lips aspect ratio
-        cv2.putText(frame, "L.A.R. : {:.2f}".format(lar), (10, 90), font, font_scale, (0, 0, 255), 2)
+            # Calculate E.A.R. and L.A.R.
+            left_ear = eye_aspect_ratio(left_eye_points)  # Left eye aspect ratio
+            right_ear = eye_aspect_ratio(right_eye_points)  # Right eye aspect ratio
+            ear = (left_ear + right_ear) / 2.0  # Average eye aspect ratio
+            cv2.putText(frame, "E.A.R. : {:.2f}".format(ear), (10, 30), font, font_scale, (0, 0, 255), 2)
 
-        # BLINK detected
-        if ear < ear_thresh:
-            blink_counter += 1
-        else:
-            if blink_counter > 3:
-                total_blinks += 1
-            blink_counter = 0
-        cv2.putText(frame, "Blinks: {}".format(total_blinks), (10, 50), font, font_scale, (0, 0, 255), 2)
+            lar = lips_aspect_ratio(lips_points)  # Lips aspect ratio
+            cv2.putText(frame, "L.A.R. : {:.2f}".format(lar), (10, 90), font, font_scale, (0, 0, 255), 2)
 
-        # YAWN detected
-        if lar > lar_thresh:
-            yawn_counter += 1
-        else:
-            if yawn_counter > 1:
-                total_yawns += 1
-            yawn_counter = 0
-        cv2.putText(frame, "Yawns: {}".format(total_yawns), (10, 110), font, font_scale, (0, 0, 255), 2)
+            # BLINK detected
+            if ear < ear_thresh:
+                blink_counter += 1
+            else:
+                if blink_counter > 3:
+                    total_blinks += 1
+                blink_counter = 0
+            cv2.putText(frame, "Blinks: {}".format(total_blinks), (10, 50), font, font_scale, (0, 0, 255), 2)
 
-        # show alert text if person yawns
-        # if total_yawns > 2 or total_blinks > 3:
-        #     # frame[20:120, w - 120:w - 20] = image  # Show coffee break image
-        #     cv2.putText(frame, "ALERT", (w - 120, 160), font, 1.2, (0, 0, 255), 4)
+            # YAWN detected
+            if lar > lar_thresh:
+                yawn_counter += 1
+            else:
+                if yawn_counter > 1:
+                    total_yawns += 1
+                yawn_counter = 0
+            cv2.putText(frame, "Yawns: {}".format(total_yawns), (10, 110), font, font_scale, (0, 0, 255), 2)
 
-    cv2.imshow('Frame', frame)
-    key = cv2.waitKey(1)
-    # if the user presses the 'esc' key, quit out
-    if key == 27:
-        break
-    # if key == ord('r') or key == ord('R'):
-    #     total_blinks, total_yawns = 0, 0  # Reset calculations by pressing 'r' or 'R'
+            # show alert text if person yawns
+            # if total_yawns > 2 or total_blinks > 3:
+            #     # frame[20:120, w - 120:w - 20] = image  # Show coffee break image
+            #     cv2.putText(frame, "ALERT", (w - 120, 160), font, 1.2, (0, 0, 255), 4)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('Frame', frame)
+        key = cv2.waitKey(1)
+        # if the user presses the 'esc' key, quit out
+        if key == 27:
+            break
+        # if key == ord('r') or key == ord('R'):
+        #     total_blinks, total_yawns = 0, 0  # Reset calculations by pressing 'r' or 'R'
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
